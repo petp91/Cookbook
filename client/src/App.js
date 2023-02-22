2083d7f4088b027a8158c855537eec1e55302a86
petp91 <115895086+petp91@users.noreply.github.com>
2023-02-22 19:51:44 +0100

    doplnění package-lock.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import HomePage from "./pages/HomePage";
-import FormPage from "./pages/FormPage";
+import RecipePage from "./pages/RecipePage";
 import {Route, Routes} from "react-router-dom";
+import AboutPage from "./pages/AboutPage";
 
 const App = () => {
     return (
         <Routes>
             <Route path="/" element={ <HomePage/> } />
-            <Route path="/form" element={ <FormPage /> } />
+            <Route path="/recipe" element={ <RecipePage /> } />
+            <Route path="/about" element={ <AboutPage /> } />
         </Routes>
     );
 };
