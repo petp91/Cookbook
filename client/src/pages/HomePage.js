d0267c49973d6c3fba7988b5c239fafe65b2597f
Jiří Hošek <7355480+jirihos@users.noreply.github.com>
2023-02-24 18:53:55 +0100

    Přidana komponenta Menu

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,8 +1,16 @@
+import Container from 'react-bootstrap/esm/Container';
+import Menu from '../components/Menu'
+import logo from '../assets/logo-400px.png'
+
 const HomePage = () => {
     return (
         <div>
+            <Menu />
             <h1>Home Page</h1>
             <p>Sem přijde menu a logo</p>
+            <Container className='d-flex justify-content-center'>
+                <img src={logo} alt='logo'></img>
+            </Container>
 
         </div>
     );
