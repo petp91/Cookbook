161c23bed62a246163b26cf23afba1d73863f2f2
jirihos <7355480+jirihos@users.noreply.github.com>
2023-03-05 20:23:23 +0100

    Drobná vylepšení

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,14 +1,10 @@
-import { Container, Row, Col, Image } from 'react-bootstrap';
+import { Container, Image } from 'react-bootstrap';
 import logo from '../assets/logo-512px.png'
 
 const HomePage = () => {
     return (
-        <Container className='d-flex justify-content-center'>
-            <Row className='justify-content-center pt-5'>
-                <Col xs='8'>
-                    <Image src={logo} fluid='true'></Image>
-                </Col>
-            </Row>
+        <Container fluid className='d-flex justify-content-center'>
+            <Image src={logo} fluid className='m-5'></Image>
         </Container>
     );
 };
