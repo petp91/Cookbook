82e8f2c333b98cd5b75bb95bb6141576f96d11da
Radek Podsclan <raycode87@gmail.com>
2023-03-07 18:17:35 +0100

    Recpety a ingreadience

--- client/src/pages/RecipePage.js
+++ client/src/pages/RecipePage.js
@@ -3,7 +3,9 @@ const RecipePage = () => {
         <div>
             <h1>Recipe Page</h1>
             <p>sem přijde btn a formulář. Zkus to přes useState nastavit aby se po kliknutí zobrazil formulář</p>
+            <RecipeModal />
         </div>
+        
     );
 };
 
