3deece88b6d4773ea54aaeaffb281ef0f5144a68
JirkaMrk <125826485+JirkaMrk@users.noreply.github.com>
2023-03-14 20:04:47 +0100

    RecipeCards, basic filtering,

--- client/src/components/MenuCard.js
+++ client/src/components/MenuCard.js
@@ -1,7 +1,5 @@
 import React from "react";
 import '../layout/MenuCard-style.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
