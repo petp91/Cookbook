5576c38dcfe150e2763d51a035d18910a7c16d21
jirihos <7355480+jirihos@users.noreply.github.com>
2023-03-16 21:43:59 +0100

    MenuAccount component improvements

--- client/src/components/MenuAccount.js
+++ client/src/components/MenuAccount.js
@@ -1,5 +1,5 @@
-import {useState} from 'react'
-import { Button, Form, Modal, ModalBody, Nav, NavDropdown, Dropdown } from 'react-bootstrap'
+import { useState } from 'react'
+import { Button, Form, Modal, Nav, NavDropdown, Dropdown } from 'react-bootstrap'
 import FormGroup from './FormGroup';
 
 const MenuAccount = () => {
@@ -8,15 +8,17 @@ const MenuAccount = () => {
     const [showLogin, setShowLogin] = useState(false);
     const [showRegister, setShowRegister] = useState(false);
 
-    const [loginFormData, setLoginFormData] = useState({
+    const defaultLoginData = {
         username: '',
         password: ''
-    });
-    const [registerFormData, setRegisterFormData] = useState({
+    };
+    const defaultRegisterData = {
         username: '',
         email: '',
         password: ''
-    });
+    };
+    const [loginFormData, setLoginFormData] = useState(defaultLoginData);
+    const [registerFormData, setRegisterFormData] = useState(defaultRegisterData);
 
     const loginHandler = e => {
         e.preventDefault();
@@ -24,14 +26,14 @@ const MenuAccount = () => {
 
         setLoggedInUsername(loginFormData.username);
         setShowLogin(false);
-        // TODO reset form
+        setLoginFormData(defaultLoginData);
     };
     const registerHandler = e => {
         e.preventDefault();
         console.log(registerFormData);
 
         setShowRegister(false);
-        // TODO reset form
+        setRegisterFormData(defaultRegisterData);
     };
     const logoutHandler = e => {
         setLoggedInUsername(null);
@@ -39,8 +41,8 @@ const MenuAccount = () => {
 
     return (
         <>
-            { loggedInUsername
-            ? <Nav className='d-inline-block ms-3'>
+            { loggedInUsername ? (
+                <Nav className='d-inline-block ms-3'>
                     <Dropdown className='nav-item'>
                         {/* This dropdown is made in a special way, so that we can use <b></b> tags in the dropdown title */}
                         <Dropdown.Toggle as='a' className='nav-link' role='button' tabIndex={0} href='#'>Logged in as <b>{loggedInUsername}</b></Dropdown.Toggle>
@@ -48,75 +50,78 @@ const MenuAccount = () => {
                             <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
                         </Dropdown.Menu>
                     </Dropdown>
-                    
-                
-            </Nav>
-            : <>
-                <Button className='ms-3' onClick={() => setShowLogin(true)}>Login</Button>
-                <Button variant='secondary' className='ms-2' onClick={() => setShowRegister(true)}>Sign-up</Button>
-            </>
-            }
+                </Nav>
+            ) : (
+                <>
+                    <Button className='ms-3' onClick={() => setShowLogin(true)}>Login</Button>
+                    <Button variant='secondary' className='ms-2' onClick={() => setShowRegister(true)}>Register</Button>
+                </>
+            )}
 
             <Modal show={showLogin} onHide={() => setShowLogin(false)}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Login</Modal.Title>
-                </Modal.Header>
-                <ModalBody>
-                    <Form onSubmit={loginHandler}>
-                        <FormGroup
-                            controlId='formBasicUsername'
-                            label='Username'
-                            type='text'
-                            placeholder='Enter username'
-                            value={loginFormData.username}
-                            onChange={e => setLoginFormData({...loginFormData, username: e.target.value})}
-                        />
-                        <FormGroup
-                            controlId='formBasicPassword'
-                            label='Password'
-                            type='password'
-                            placeholder='Enter password'
-                            value={loginFormData.password}
-                            onChange={e => setLoginFormData({...loginFormData, password: e.target.value})}
-                        />
+                <Form onSubmit={loginHandler}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Login</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                            <FormGroup
+                                controlId='formBasicUsername'
+                                label='Username'
+                                type='text'
+                                placeholder='Enter username'
+                                value={loginFormData.username}
+                                onChange={e => setLoginFormData({...loginFormData, username: e.target.value})}
+                            />
+                            <FormGroup
+                                controlId='formBasicPassword'
+                                label='Password'
+                                type='password'
+                                placeholder='Enter password'
+                                value={loginFormData.password}
+                                onChange={e => setLoginFormData({...loginFormData, password: e.target.value})}
+                            />
+                    </Modal.Body>
+                    <Modal.Footer>
                         <Button type='submit'>Login</Button>
-                    </Form>
-                </ModalBody>
+                    </Modal.Footer>
+                </Form>
             </Modal>
 
             <Modal show={showRegister} onHide={() => setShowRegister(false)}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Registration</Modal.Title>
-                </Modal.Header>
-                <ModalBody>
-                    <Form onSubmit={registerHandler}>
-                        <FormGroup
-                            controlId='formBasicUsername'
-                            label='Username'
-                            type='text'
-                            placeholder='Enter username'
-                            value={registerFormData.username}
-                            onChange={e => setRegisterFormData({...registerFormData, username: e.target.value})}
-                        />
-                        <FormGroup
-                            controlId='formBasicEmail'
-                            label='Email'
-                            type='email'
-                            placeholder='Enter email'
-                            value={registerFormData.email}
-                            onChange={e => setRegisterFormData({...registerFormData, email: e.target.value})}
-                        />
-                        <FormGroup
-                            controlId='formBasicPassword'
-                            label='Password'
-                            type='password'
-                            placeholder='Enter password'
-                            value={registerFormData.password}
-                            onChange={e => setRegisterFormData({...registerFormData, password: e.target.value})}
-                        />
+                <Form onSubmit={registerHandler}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Registration</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                            <FormGroup
+                                controlId='formBasicUsername'
+                                label='Username'
+                                type='text'
+                                placeholder='Enter username'
+                                value={registerFormData.username}
+                                onChange={e => setRegisterFormData({...registerFormData, username: e.target.value})}
+                            />
+                            <FormGroup
+                                controlId='formBasicEmail'
+                                label='Email'
+                                type='email'
+                                placeholder='Enter email'
+                                value={registerFormData.email}
+                                onChange={e => setRegisterFormData({...registerFormData, email: e.target.value})}
+                            />
+                            <FormGroup
+                                controlId='formBasicPassword'
+                                label='Password'
+                                type='password'
+                                placeholder='Enter password'
+                                value={registerFormData.password}
+                                onChange={e => setRegisterFormData({...registerFormData, password: e.target.value})}
+                            />
+                    </Modal.Body>
+                    <Modal.Footer>
                         <Button type='submit'>Register</Button>
-                    </Form>
-                </ModalBody>
+                    </Modal.Footer>
+                </Form>
             </Modal>
         </>
     )
