4cc0f713b813858095b7796ea469d750b0d8ad60
jirihos <7355480+jirihos@users.noreply.github.com>
2023-03-24 17:42:38 +0100

    Fixed and cleaned up recipe details

--- client/src/components/RecipeModal.js
+++ client/src/components/RecipeModal.js
@@ -1,10 +1,6 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-
-const ModalPizzaName = {
-    textAlign: "center",
-  }
 
 function RecipeModal() {
     const [show, setShow] = useState(false);
@@ -13,9 +9,7 @@ function RecipeModal() {
 
     return (
         <>
-        <div className="position-absolute top-50 translate-middle">
-            <Button variant="primary" size="lg" onClick={handleShow}>Details</Button>
-            </div>
+            <Button className='w-100' variant='outline-success' onClick={handleShow}>Details</Button>
 
             <Modal show={show} onHide={handleShow} size="xl">
                 <Modal.Header closeButton>
@@ -23,9 +17,9 @@ function RecipeModal() {
                 </Modal.Header>
                 <Modal.Body>
                     <div style={{ display: 'flex', flexDirection: 'row'}}>
-                        <div controlId="RecipeModalPicture" style={{ flex: '1 1 150rem' , backgroundColor: 'blue'}}></div>
-                        <div controlId="RecipeModalDesc" style={{ marginLeft: '5px', backgroundColor:'#FFFFFF'}}>
-                            <h1 style={ModalPizzaName}>Jméno pizzy</h1>
+                        <div style={{ flex: '1 1 150rem' , backgroundColor: 'blue'}}></div>
+                        <div style={{ marginLeft: '5px', backgroundColor:'#FFFFFF'}}>
+                            <h1 style={{textAlign: "center"}}>Jméno pizzy</h1>
                             <ul>
                                 <li>Lorem</li>
                                 <li>ipsum</li>
@@ -48,35 +42,7 @@ function RecipeModal() {
                 </Modal.Body>
                 <Modal.Footer>
                 </Modal.Footer>
-            </Modal> 
-
-{/*    {show === false? "": <Card style={{ width: '18rem' }} show={show} onHide={handleShow}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-                          <div style={ModalWindowCSS}>
-                        <div controlId="RecipeModalPicture" style={ModalPictureCSS}></div>
-                        <div controlId="RecipeModalDesc" style={ModalDescriptionCSS}>
-                            <h1 style={ModalPizzaName}>Jméno pizzy</h1>
-                            <ul>
-                                <li>Lorem</li>
-                                <li>ipsum</li>
-
-
-                            </ul>
-                            <p>Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
-                            
-                            np
-                            </div>
-                            </div>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>}
-    */}
+            </Modal>
         </>
     );
 }
