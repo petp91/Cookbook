84e4eaea22c98dbc476911cd0ce87198f5b07d01
JirkaMrk <125826485+JirkaMrk@users.noreply.github.com>
2023-04-01 12:46:41 +0200

    loading spinner recipe page
    loading spinner recipe page

--- client/src/components/MenuCardsGrid.js
+++ client/src/components/MenuCardsGrid.js
@@ -8,30 +8,65 @@ import Col from 'react-bootstrap/Col';
 const MenuCardsOutput = () => {
     const [item, setItem] = useState([]);
 
+    const [ServerCall, setServerCall] = useState({
+        state: "pending",
+    });
+
     useEffect(()=> {
         axios.get('http://localhost:8080/api/recipes')
             .then((response)=> {
                 console.log(response);
                 setItem(response.data);
+                setServerCall({ state: "success"});
             })
+            .catch(function (error) {
+                setServerCall({ state: "error"});
+            });
     }, [])
-    return (
-        <>
-            <Container>
-                <Row>
-                    {item.map((recipe) => {
-                        return (
-                            <Col key={recipe._id} className='d-flex justify-content-center' md={6} lg={4} xl={4} xxl={3}>
-                                <RecipeMenuCard recipe={recipe}/>
-                            </Col>
-                        )
-                    })}
-                </Row>
-            </Container>
-        </>
-    );
-};
 
+    function serverResponseState() {
+        switch (ServerCall.state) {
+            case "pending":
+                return (
+                    <div className="text-center">
+                        <button className="btn btn-success" type="button" disabled>
+                            <span className="spinner-border spinner-border-lg" role="status" aria-hidden="true"></span>
+                            ...loading...
+                        </button>
+                    </div>
 
+                );
+            case "success":
+                return (
+                    <>
+                        <Container>
+                            <Row>
+                                {item.map((recipe) => {
+                                    return (
+                                        <Col key={recipe._id} className='d-flex justify-content-center' md={6} lg={4} xl={4} xxl={3}>
+                                            <RecipeMenuCard recipe={recipe}/>
+                                        </Col>
+                                    )
+                                })}
+                            </Row>
+                        </Container>
+                    </>
+                );
+            case "error":
+                return (
+                    <div className="text-center">
+                       <button className="btn btn-danger" type="button">
+                          <span className="spinner-border spinner-border-lg" role="status" aria-hidden="true"></span>
+                          ...server not responding...
+                      </button>
+                    </div>
+                );
+            default:
+                return null;
+        }
+    }
+
+    return <div>{serverResponseState()}</div>;
+}
 
 export default MenuCardsOutput;
