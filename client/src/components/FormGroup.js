bab80e8e79f9e497113ce1dd503e1068293236ba
jirihos <7355480+jirihos@users.noreply.github.com>
2023-04-03 22:59:34 +0200

    Simplified FormGroup component

--- client/src/components/FormGroup.js
+++ client/src/components/FormGroup.js
@@ -1,10 +1,10 @@
 import { Form } from 'react-bootstrap'
 
-const FormGroup = ({ controlId, label, as, type, placeholder, rows, value, setValue }) => {
+const FormGroup = ({ controlId, label, setValue, ...rest }) => {
     return (
         <Form.Group className='mb-3' controlId={controlId}>
             <Form.Label>{label}</Form.Label>
-            <Form.Control as={as} type={type} placeholder={placeholder} rows={rows} value={value} onChange={(event) => setValue(event.target.value)} />
+            <Form.Control onChange={(event) => setValue(event.target.value)} {...rest} />
         </Form.Group>
     )
 }
