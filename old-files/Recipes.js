c5d6ab3b845ed0dfdb11c5e4f71506590bed2168
jirihos <7355480+jirihos@users.noreply.github.com>
2023-05-16 19:10:47 +0200

    Removed MongoDB code

--- old-files/Recipes.js
+++ old-files/Recipes.js
@@ -1,16 +1,3 @@
-const mongoose = require('mongoose');
-
-const RecipeSchema = new mongoose.Schema({
-    id: { type: String, required: true },
-    displayName: { type: String, required: true },
-    displayImage: { type: String },
-    size: { type: String, required: true },
-    description: { type: String },
-    ingredients: { type: Object, required: true },
-});
-
-const Recipe = mongoose.model('Recipe', RecipeSchema);
-
 const pizzaRecipes = [
     {
         id: "PIZZARECIPE001",
@@ -388,6 +375,5 @@ const pizzaRecipes = [
 ];
 
 module.exports = {
-    Recipe,
-    pizzaRecipes,
+    pizzaRecipes
 };
