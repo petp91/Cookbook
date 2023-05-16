9820e58be97a41c83db631c05eef86503d3bb657
Martin Nebehay <martinnebe456@gmail.com>
2023-05-16 18:35:36 +0200

    HomePage, AboutPage, NavBarS

--- client/src/pages/AboutPage.js
+++ client/src/pages/AboutPage.js
@@ -1,5 +1,40 @@
+import React from 'react';
+import '../layout/AboutPage.css';
+
 const AboutPage = () => {
-    return <h1>About page</h1>
+  return (
+    <div className="about">
+      <header className="header">
+        <h1 className="title">About Pizza Recipes</h1>
+      </header>
+
+      <section className="content">
+        <div className="description">
+          <h2 className="subtitle">Delicious Pizza Recipes</h2>
+          <p className="text">
+          At Pizza Recipes, we are obsessed with pizza! Our mission is to bring the joy of pizza-making into your kitchen by providing an extensive collection of mouthwatering pizza recipes. From traditional classics to unique and innovative creations, we strive to inspire your culinary creativity and satisfy your pizza cravings.          </p>
+          <p className="text">
+          Our team of pizza aficionados scours the culinary world to handpick the best recipes for you. Each recipe undergoes rigorous testing and refinement to ensure it delivers exceptional taste and quality. We take pride in offering recipes that are approachable yet exciting, enabling both beginners and experienced cooks to craft pizzas that rival those from the finest pizzerias.
+          </p>
+          <p className="text">
+          In addition to our diverse recipe collection, we provide valuable resources to enhance your pizza-making journey. Our blog features informative articles, expert tips, and techniques to help you master the art of pizza-making. We also showcase pizza-related news, trends, and stories from around the globe, keeping you connected to the vibrant world of pizza.
+          </p>
+          <p className='text'>
+          At Pizza Recipes, we believe that pizza brings people together. It's not just a meal; it's a shared experience that creates lasting memories. That's why we encourage you to gather your loved ones, roll up your sleeves, and embark on a culinary adventure together. Our recipes are designed to foster creativity, allowing you to personalize your pizzas with your favorite toppings, sauces, and cheeses.
+          </p>
+          <p className='text'>
+          Join our passionate community of pizza enthusiasts and embark on a journey of dough-stretching, sauce-saucing, and cheese-melting goodness. Whether you're a pizza lover looking to expand your repertoire or a beginner eager to learn, Pizza Recipes is your go-to resource for all things pizza.
+          </p>
+          <p className='text'>
+          We invite you to explore our recipe collections, discover new flavors and techniques, and share your own pizza creations with us. Let's celebrate the joy of pizza and elevate your homemade pizza game to new heights!
+          </p>
+          <p className='text'>
+          Get ready to unlock the secrets of perfect crusts, tantalizing toppings, and irresistible aromas. Pizza Recipes is here to make your pizza dreams come true, one slice at a time.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default AboutPage;
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,11 +1,113 @@
-import { Container, Image } from 'react-bootstrap';
-import logo from '../assets/logo-512px.png'
+//import { Container, Image } from 'react-bootstrap';
+//import logo from '../assets/logo-512px.png'
+
+import "../layout/HomePage.css";
+import { Link } from 'react-router-dom';
+
+
 
 const HomePage = () => {
+
     return (
-        <Container fluid className='d-flex justify-content-center'>
-            <Image src={logo} fluid className='m-5'></Image>
-        </Container>
+        <div className="home-page">
+            <header className="hero">
+                <h1 className="title">Welcome to Our Website</h1>
+                <p className="subtitle">Discover these amazing pizza recipes.</p>
+                <Link to="/recipes" className="cta-button">
+                    Get Started
+                </Link>
+            </header>
+
+            <section className="features">
+                <div className="feature">
+                    <i className="fas fa-search"></i>
+                    <h3 className="feature-title">Search Recipes</h3>
+                    <p className="feature-description">Find delicious recipes for any occasion</p>
+                </div>
+                <div className="feature">
+                    <i className="fas fa-book"></i>
+                    <h3 className="feature-title">Recipe Collections</h3>
+                    <p className="feature-description">Explore curated collections of recipes</p>
+                </div>
+                <div className="feature">
+                    <i className="fas fa-heart"></i>
+                    <h3 className="feature-title">Save Favorites</h3>
+                    <p className="feature-description">Save your favorite recipes for quick access</p>
+                </div>
+            </section>
+
+            <section className="testimonial">
+                <h3 className="testimonial-title">Reviews from our users:</h3>
+                <div className="testimonial-table">
+
+                    <div className="testimonial-row">
+                        <div className="testimonial-cell">
+                            <div className="testimonial-content">
+                                <p className="quote">"The pizza recipes on this site are amazing! I've tried several and they always turn out delicious."</p>
+                                <p className="author">- John Smith</p>
+                            </div>
+                        </div>
+                        <div className="testimonial-cell">
+                            <div className="testimonial-content">
+                                <p className="quote">"As a pizza lover, I appreciate the variety of recipes available here. It's like a pizza paradise!"</p>
+                                <p className="author">- Emily Johnson</p>
+                            </div>
+                        </div>
+                        <div className="testimonial-cell">
+                            <div className="testimonial-content">
+                                <p className="quote">"I've been searching for the perfect pizza dough recipe, and I finally found it on this site. Highly recommended!"</p>
+                                <p className="author">- David Anderson</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="testimonial-row">
+                        <div className="testimonial-cell">
+                            <div className="testimonial-content">
+                                <p className="quote">"The step-by-step instructions make it easy to follow the recipes. Thanks to this site, I can now make homemade pizza like a pro."</p>
+                                <p className="author">- Sarah Thompson</p>
+                            </div>
+                        </div>
+                        <div className="testimonial-cell">
+                            <div className="testimonial-content">
+                                <p className="quote">"I'm a fan of thin-crust pizza, and this site has some fantastic thin-crust recipes. My family loves it!"</p>
+                                <p className="author">- Michael Davis</p>
+                            </div>
+                        </div>
+                        <div className="testimonial-cell">
+                            <div className="testimonial-content">
+                                <p className="quote">"The toppings combinations on this site are creative and delicious. I've discovered new flavor combinations I never thought of before."</p>
+                                <p className="author">- Jennifer Rodriguez</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="testimonial-row">
+                        <div className="testimonial-cell">
+                            <div className="testimonial-content">
+                                <p className="quote">"Being able to customize and create your own pizza recipe is a great feature. It's fun experimenting with different flavors."</p>
+                                <p className="author">- Andrew Wilson</p>
+                            </div>
+                        </div>
+                        <div className="testimonial-cell">
+                            <div className="testimonial-content">
+                                <p className="quote">"The site provides useful tips and tricks for achieving the perfect pizza. My pizzas have improved significantly since I started using this resource."</p>
+                                <p className="author">- Jessica Lee</p>
+                            </div>
+                        </div>
+                        <div className="testimonial-cell">
+                            <div className="testimonial-content">
+                                <p className="quote">"The site has a great selection of gluten-free pizza recipes. Finally, I can enjoy a tasty pizza without worrying about gluten."</p>
+                                <p className="author">- Christopher Martinez</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+
+
+        </div>
     );
 };
 
