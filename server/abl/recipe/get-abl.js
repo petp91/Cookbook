a29959bde81c618e9dd81c2fd06a03cfecda8550
jirihos <7355480+jirihos@users.noreply.github.com>
2023-06-03 19:12:19 +0200

    Get by ID improvements

--- server/abl/recipe/get-abl.js
+++ server/abl/recipe/get-abl.js
@@ -3,8 +3,16 @@ const recipeDao = require('../../dao/file_storage/recipe-dao');
 async function GetAbl(req, res) {
     try {
         let id = req.params.id;
+
         let recipe = await recipeDao.getRecipe(id);
-        res.send(recipe);
+        
+        if (recipe) {
+            res.send(recipe);
+        } else {
+            res.status(404).send({
+                errors: [`a recipe with the id ${id} was not found`]
+            });
+        }
     } catch (e) {
         console.error(e);
         res.status(500).send({
